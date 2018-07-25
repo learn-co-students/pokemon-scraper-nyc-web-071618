3005d2cf106b2593e06d4d556e975749529d3030
chejianchao <820992376@qq.com>
2018-07-25 14:11:55 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,20 @@
 class Pokemon
+  attr_reader :id, :name, :type, :db
+  def initialize(id:,name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    #db.execute("insert into pokemon(id, name, type) values(?,?,?)", id, name, type)
+  end
+  
+  
+  def self.save(name, type, db)
+    db.execute("insert into pokemon(name, type) values(?,?)",name, type)
+  end
+  
+  def self.find(id, db)
+    #db.results_as_hash = true
+    res = db.execute("select * from pokemon where id =?", id)[0]
+    self.new({id: res[0], name: res[1], type: res[2], db: db})
+  end
 end
